612ca5f6bed18b708d8dd120ec0685d74c3ba45e
nischal <nischalkshaj5@gmail.com>
2023-09-21 05:11:07 +0530

    commit number 4

---
+++ Database/connection.js
@@ -0,0 +1,15 @@
+// js file where the database is connected
+
+// importing the modules for connecting to the mongodb server
+const mongoose = require('mongoose')
+
+// connecting to the mongodb
+mongoose.connect("mongodb://localhost:27017/stepCrazyUser")
+.then(()=>{
+    console.log("Connection Success....");
+})
+.catch(()=>{
+    console.log("Connection error....");
+})
+
+module.exports = mongoose;
--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const express  = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const multer = require('multer')
+const multer = require('multer');
 
 // requiring the local modules for the project
-const routerHome = require('./routes/userRoutes/home')
-const userDb = require('./models/user/userDatabase')
+const routerHome = require('./routes/userRoutes/home');
+const mongose = require('./Database/connection');
+const userDb = require('./models/user/userDatabase');
 
 // setting the express() as app
 const app = express();
@@ -15,25 +16,12 @@ const app = express();
 // setting up the port number
 const port = 4000;
 
-// connecting mongodb server
-mongoose.connect("mongodb://localhost:27017/stepCrazyUser")
-.then(()=>{
-    console.log("Connection Success....");
-})
-.catch(()=>{
-    console.log("Connection error....");
-})
-
 // setting up the ejs page
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname,'views'))
 
-
-
 // setting up the routes
 app.use('/',routerHome)
-
-
 
 // running the project in the specified port number
 app.listen(port,()=>{
