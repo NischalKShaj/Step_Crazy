e7e585d3ab87d557b2a767b17e018dcf5f92239a
nischal <nischalkshaj5@gmail.com>
2023-11-12 19:17:40 +0530

    91st commit for the project

--- controller/admincontroller/homeController.js
+++ controller/admincontroller/homeController.js
@@ -12,9 +12,57 @@ exports.getAdminHome = async (req, res) => {
   const admin = await collection.findOne({ email: adminId });
   try {
     if (admin) {
-     const report = await reportCollection.find().count();
-     console.log(report);
-     res.render("admin/admindashboard", {report})
+      const orders = await reportCollection.find(
+        {},
+        { "orderDetails.date": 1 }
+      );
+
+      // Initialize an object to store the counts for each month
+      const monthlyCounts = {};
+
+      // Count the reports for each month
+      orders.forEach((order) => {
+        order.orderDetails.forEach((orderDetail) => {
+          const month =
+            orderDetail.date instanceof Date
+              ? orderDetail.date.toLocaleString("default", { month: "long" })
+              : "Invalid Date";
+
+          // Increment the count for the month
+          monthlyCounts[month] = (monthlyCounts[month] || 0) + 1;
+        });
+      });
+
+      // Extract months and counts for chart data
+      const months = Object.keys(monthlyCounts);
+      const counts = Object.values(monthlyCounts);
+
+      console.log("Monthly Counts:", monthlyCounts);
+
+      const yearlyCounts = {};
+      orders.forEach((order) => {
+        order.orderDetails.forEach((orderDetail) => {
+          const year =
+            orderDetail.date instanceof Date
+              ? orderDetail.date.getFullYear()
+              : null;
+
+          if (year !== null) {
+            if (!yearlyCounts[year]) {
+              yearlyCounts[year] = 0;
+            }
+
+            yearlyCounts[year]++;
+          }
+        });
+      });
+
+      // Extract years and counts from the result
+      const years = Object.keys(yearlyCounts).map((year) => parseInt(year));
+      const count = Object.values(yearlyCounts);
+
+      // Render the page with the data
+      res.render("admin/admindashboard", { months, counts, years, count });
     }
   } catch (error) {
     console.log("error", error);
@@ -37,10 +85,57 @@ exports.postAdminHome = async (req, res) => {
     ) {
       req.session.admin = req.body.email;
       console.log("inside the dashboard...");
-      const report = await reportCollection.find().count();
-      console.log("report", report);
-      // res.json({report})
-      res.render("admin/admindashboard",{report})
+      const orders = await reportCollection.find(
+        {},
+        { "orderDetails.date": 1 }
+      );
+
+      // Initialize an object to store the counts for each month
+      const monthlyCounts = {};
+
+      // Count the reports for each month
+      orders.forEach((order) => {
+        order.orderDetails.forEach((orderDetail) => {
+          const month =
+            orderDetail.date instanceof Date
+              ? orderDetail.date.toLocaleString("default", { month: "long" })
+              : "Invalid Date";
+
+          // Increment the count for the month
+          monthlyCounts[month] = (monthlyCounts[month] || 0) + 1;
+        });
+      });
+
+      // Extract months and counts for chart data
+      const months = Object.keys(monthlyCounts);
+      const counts = Object.values(monthlyCounts);
+
+      console.log("Monthly Counts:", monthlyCounts);
+
+      const yearlyCounts = {};
+      orders.forEach((order) => {
+        order.orderDetails.forEach((orderDetail) => {
+          const year =
+            orderDetail.date instanceof Date
+              ? orderDetail.date.getFullYear()
+              : null;
+
+          if (year !== null) {
+            if (!yearlyCounts[year]) {
+              yearlyCounts[year] = 0;
+            }
+
+            yearlyCounts[year]++;
+          }
+        });
+      });
+
+      // Extract years and counts from the result
+      const years = Object.keys(yearlyCounts).map((year) => parseInt(year));
+      const count = Object.values(yearlyCounts);
+
+      // Render the page with the data
+      res.render("admin/admindashboard", { months, counts, years, count });
     } else {
       const message = "Invalid admin credentials";
       res.redirect(`/admin?success=${encodeURIComponent(message)}`);
--- controller/admincontroller/loginController.js
+++ controller/admincontroller/loginController.js
@@ -1,15 +1,61 @@
 // this shows the admins login
 // modules required for the admin login
 const collection = require("../../models/admin/adminDatabase");
-const reportCollection = require("../../models/reports/reportDetails")
+const reportCollection = require("../../models/reports/reportDetails");
 
 // for rendering the admin page
 exports.getAdminPage = async (req, res) => {
   const admin = req.session.admin;
-  console.log("admin",admin);
+  console.log("admin", admin);
   if (admin) {
-    const report = await reportCollection.find().count();
-    res.render("admin/admindashboard",{report});
+    const orders = await reportCollection.find({}, { "orderDetails.date": 1 });
+
+    // Initialize an object to store the counts for each month
+    const monthlyCounts = {};
+
+    // Count the reports for each month
+    orders.forEach((order) => {
+      order.orderDetails.forEach((orderDetail) => {
+        const month =
+          orderDetail.date instanceof Date
+            ? orderDetail.date.toLocaleString("default", { month: "long" })
+            : "Invalid Date";
+
+        // Increment the count for the month
+        monthlyCounts[month] = (monthlyCounts[month] || 0) + 1;
+      });
+    });
+
+    // Extract months and counts for chart data
+    const months = Object.keys(monthlyCounts);
+    const counts = Object.values(monthlyCounts);
+
+    console.log("Monthly Counts:", monthlyCounts);
+
+    const yearlyCounts = {};
+    orders.forEach((order) => {
+      order.orderDetails.forEach((orderDetail) => {
+        const year =
+          orderDetail.date instanceof Date
+            ? orderDetail.date.getFullYear()
+            : null;
+
+        if (year !== null) {
+          if (!yearlyCounts[year]) {
+            yearlyCounts[year] = 0;
+          }
+
+          yearlyCounts[year]++;
+        }
+      });
+    });
+
+    // Extract years and counts from the result
+    const years = Object.keys(yearlyCounts).map((year) => parseInt(year));
+    const count = Object.values(yearlyCounts);
+
+    // Render the page with the data
+    res.render("admin/admindashboard", { months, counts, years, count });
   } else {
     const invalid = req.query.success;
     console.log(invalid);
